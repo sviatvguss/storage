31ae764c4d69e4d8ecba239361a4925f81f43386
sviatvguss <sviat.v.guss@gmail.com>
2022-06-06 18:09:01 +0600

    Storage project example with Project Layout (/cmd, /internal, /pkg) (https://github.com/golang-standards/project-layout)

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,6 +2,6 @@ package storage
 
 import "github.com/sviatvguss/storage/internal/storage"
 
-func NewStorage(someArg string) *storage.Storage {
+func NewStorage() *storage.Storage {
 	return storage.NewStorage()
 }
